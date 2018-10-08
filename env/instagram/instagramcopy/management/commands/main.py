c7d36ac6bfe03369208cdd88bfbf3771d5a3a9ac
andreamcm <cor16076@uvg.edu.gt>
2018-10-08 12:01:18 -0600

    Arreglado

--- env/instagram/instagramcopy/management/commands/main.py
+++ env/instagram/instagramcopy/management/commands/main.py
@@ -10,16 +10,35 @@ from django.core.management.base import BaseCommand
 #from commands import *
 from django.contrib.auth.models import User
 
+# Se definen las funciones de los menus a utilizar
+def menu_principal():
+    print ("Bienvenido a InstagramCopy")
+    print ("Puede seleccionar cualquiera de las siguientes opciones")
+    print ("1. Registarse\n2. Ver usuarios\n3. Acceder\n4. Salir")
+    eleccion = input("¿Qué acción desea realizar?: ")
+
+
+def menu_usuario():
+    print ("Bienvenido " + User.objects.get(first_name))
+    print ("Puede realizar lo siguiente: ")
+    print ("1. Hacer un post\n2. Ver todos los posts\n3. Borrar un post\n4. Salir")
+    eleccion_usuario = input("¿Qué acción desea realizar?")
+
 # Se da al usuario la bienvenida al programa
 print ("Bienvenido a InstagramCopy")
-print ("Puede seleccionar cualquiera de las siguientes opciones")
-print ("1. Registarse\n2. Ver usuarios\n3. Acceder\n4. Salir")
-eleccion = input("¿Qué acción desea realizar?: ")
+#menu_principal()
+#print ("Bienvenido a InstagramCopy")
+#print ("Puede seleccionar cualquiera de las siguientes opciones")
+#print ("1. Registarse\n2. Ver usuarios\n3. Acceder\n4. Salir")
+#eleccion = input("¿Qué acción desea realizar?: ")
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        while True:
-            if (eleccion == "1"):
+        while (True):
+            print ("Puede seleccionar cualquiera de las siguientes opciones")
+            print ("1. Registarse\n2. Ver usuarios\n3. Acceder\n4. Salir")
+            eleccion = input("¿Qué acción desea realizar?: ")
+            if (eleccion == '1'):
                 print ("Cantidad antes:", User.objects.all().count())
                 nombre = input("Ingrese su primer nombre: ")
                 apellido = input("Ingrese su primer apellido: ")
@@ -28,11 +47,9 @@ class Command(BaseCommand):
                 mi_usuario = User(first_name=nombre, last_name=apellido, username=username, email=email)
                 mi_usuario.save()
                 print ("Cantidad despues:", User.objects.all().count())
-                break
             elif (eleccion == '2'):
                 for user in User.objects.all():
                     print ("pk={}: {} {} - {}".format(user.pk, user.first_name, user.last_name, user.username, user.email))
-                break
             elif (eleccion == '3'):
                 usuario = input('Ingrese su nombre de usuario: ')
                 correo = input('Ingrese su correo electronico:')
@@ -40,7 +57,6 @@ class Command(BaseCommand):
                     acceso = User.objects.get(username = usuario, email = correo)
                 except:
                     print ('Este usuario no existe. Intentelo de nuevo')
-                break
 
             elif (eleccion == '4'):
                 print ("Gracias por usar InstagramCopy")
